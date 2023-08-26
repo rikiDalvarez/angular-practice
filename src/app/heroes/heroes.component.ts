134cb3e9f06667c614ffd0287e9d3ba6a958473e
Ricardo <ric.dalvarez@hotmail.com>
2023-08-26 16:32:48 +0200

    ngFor

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss'],
+})
+export class HeroesComponent {
+  heroes: Array<Hero> = HEROES;
+}
